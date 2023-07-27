bae862e52d1504dbfdf3005c5ec93c6ec32afde5
chaimaaSafi <safichaimaa098@gmail.com>
2023-07-27 13:35:21 +0100

    fix: commit error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,14 @@
 import "@/styles/globals.css";
-<<<<<<< Updated upstream
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-=======
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
->>>>>>> Stashed changes
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-<<<<<<< Updated upstream
-  title: 'Custom code',
-  description: 'Find your code easily',
-}
-=======
   title: "Custom code",
   description: "Find your code easily",
 };
->>>>>>> Stashed changes
 
 export default function RootLayout({
   children,
