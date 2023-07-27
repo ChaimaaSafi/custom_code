7337f934bdc34536e0ac8d5f594a8b5f7b118b76
chaimaaSafi <safichaimaa098@gmail.com>
2023-07-27 17:05:36 +0100

    feat: test mdx

---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-7TL3Y57J.mjs
@@ -0,0 +1,50 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeCodeTitles from "rehype-code-titles";
+import rehypePrism from "rehype-prism-plus";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  contentType: "mdx",
+  filePathPattern: "posts/*.mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (post) => post._raw.sourceFileName.replace(/\.mdx$/, "")
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  // Location of source files for all defined documentTypes
+  contentDirPath: "src/app/content",
+  documentTypes: [Post],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      rehypeCodeTitles,
+      rehypePrism,
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["anchor"]
+          }
+        }
+      ]
+    ]
+  }
+});
+export {
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-7TL3Y57J.mjs.map
