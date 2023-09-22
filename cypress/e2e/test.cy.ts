4ee84c26e433b7cb225d7533b6c2cfee5b5f952b
chaimaaSafi <safichaimaa098@gmail.com>
2023-09-22 02:14:50 +0100

    Allow a user to login successfully

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Login Page Test', () => {
+  // Visit the login page before each test
+  beforeEach(() => {
+    cy.visit('/Login'); // Change the URL as needed
+  });
+
+  it('Should allow a user to login successfully', () => {
+    cy.get('input[name="email"]').type("chaimaa@test.com")
+    cy.get('input[name="password"]').type("123456")
+    cy.get('button[type="submit"]').click()
+  })  
 })
